027de1baf71e5d1708df6cd7c1b79155549963b8
mararo-ops <danielmararo1@gmail.com>
2024-01-22 08:14:53 +0300

    class restaurantpizza

--- app/models.py
+++ app/models.py
@@ -35,3 +35,26 @@ class Pizza(db.Model):
 
     def __repr__(self):
         return f'<Pizza {self.id}: {self.name}'
+
+class RestaurantPizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+    def __repr__(self):
+        return f'<RestaurantPizza {self.id}: Price: {self.price}>'
+
+    def validate(self):
+        errors = {}
+
+        if not self.price:
+            errors['price'] = 'Price is required'
+
+        if self.price < 1 or self.price > 30:
+            errors['price'] = 'Price must be between 1 and 30'
+
+        return errors
