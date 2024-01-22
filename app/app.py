27e15785fefec52510d7120de25e47049e7fa3b7
mararo-ops <danielmararo1@gmail.com>
2024-01-22 08:28:04 +0300

    route to get all restaurants

--- app/app.py
+++ app/app.py
@@ -10,3 +10,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
+# Route to get all restaurants
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    restaurant_list = []
+
+    for restaurant in restaurants:
+        restaurant_data = {
+            'id': restaurant.id,
+            'name': restaurant.name,
+            'address': restaurant.address
+        }
+
+        restaurant_list.append(restaurant_data)
+
+    return jsonify(restaurant_list), 200    
+
