13ff7d8465697450677d439290e75cd05362a96f
mararo-ops <danielmararo1@gmail.com>
2024-01-22 08:18:33 +0300

    seeding the pizzas

--- app/seed.py
+++ app/seed.py
@@ -22,3 +22,13 @@ with app.app_context():
 
     db.session.add_all(restaurants)
     db.session.commit()
+
+    # Seeding the pizzas
+    pizzas = []
+    pizza_names = [
+        'Classic Margherita', 'Pepperoni Lover\'s', 'Vegetarian Delight', 'Supreme Feast',
+        'Mushroom Madness', 'BBQ Ranch Chicken', 'Spicy Sausage', 'Savory Seafood', 'Pesto Perfection',
+        'Buffalo Ranch', 'Ultimate Veggie', 'Quattro Formaggi', 'Carnivore\'s Dream',
+        'Plant-Based Bliss', 'Garlic White Pizza', 'Taco Fiesta', 'Greek Mediterranean', 'Sweet and Spicy BBQ',
+        'Bacon & Mushroom Deluxe', 'Spinach and Artichoke'
+    ]
