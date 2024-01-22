1dc54f681612a66ffec308a8bbf680eda0c515ed
mararo-ops <danielmararo1@gmail.com>
2024-01-22 08:26:24 +0300

    database URL

--- app/app.py
+++ app/app.py
@@ -1,3 +1,12 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from models import db, Restaurant, Pizza, RestaurantPizza
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza_restaurants.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
